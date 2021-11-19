9c4d942c495cae385f86ec5a0e58c284a318359b
Ash Mohan <ashwath.mohan@docusign.com>
2021-11-19 12:17:52 -0800

    updating boundary conditions

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -26,7 +26,7 @@ def go(args):
     df = pd.read_csv(artifact_local_path)
 
     logger.info("Cleaning data..")
-    idx = df['price'].between(args.min_price, args.max_price)
+    idx = df['price'].between(args.min_price, args.max_price) & df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
     df['last_review'].fillna(df['last_review'].min(), inplace=True)
